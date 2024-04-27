bd4ab0dae305894205179e9d75122aa1e8930e2e
Supriyo Maity <supriyo.codes@gmail.com>
2024-04-27 15:20:14 +0530

    second commit

--- task4/server/index.js
+++ task4/server/index.js
@@ -1,12 +1,22 @@
-const express=require('express')
-const app=express()
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+const port = 3000;
 
 
-app.get('/',(req,res)=>{
-    res.send("hello world");
-})
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+const corsOptions = {
+  origin: 'http://localhost:5173',
+  methods: "GET, POST, DELETE, PUT, PATCH, HEAD",
+  Credential: true,
+}
+app.use(cors(corsOptions))
 
 
-app.listen(3000,()=>{
-    console.log(`server is running on port 3000`)
-})
+app.use('', require("./routes/user_routes"));
+
+app.listen(port, () => {
+  console.log("Server is running at http://localhost:3000");
+});
